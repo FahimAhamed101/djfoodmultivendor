fc2ccdcd1de59595fed342463a111a88631984d3
fahim <fahimahamedweb@gmail.com>
2024-09-26 23:56:06 +0600

    marketplace added

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'vendor',
     'menu',
     'customers',
-   
+   'marketplace',
 ]
 
 MIDDLEWARE = [
---
+++ marketplace/views.py
@@ -0,0 +1,4 @@
+
+def marketplace(request):
+    
+    return render(request, "listings.html")
