afcac800af086186bb89dc41cb60b37358552991
fahim <fahimahamedweb@gmail.com>
2024-09-30 00:48:51 +0600

    vercel deploy

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -134,11 +134,11 @@ MEDIA_URL = 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email Configurations
-EMAIL_HOST = config('EMAIL_HOST')
+"""EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')"""
 
 AUTH_USER_MODEL = "accounts.User"
